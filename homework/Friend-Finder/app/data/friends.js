fa17bdba0a7ae0f7e2664da67bf94ec4d025ceda
rvanhorn <41927843+rvanhorn@users.noreply.github.com>
2018-11-17 23:59:37 -0800

    Fix score checking.

--- homework/Friend-Finder/app/data/friends.js
+++ homework/Friend-Finder/app/data/friends.js
@@ -1,8 +1,18 @@
-const users = [
+let users = [
 	{
 		name: "John Wick",
 		image: "https://cdn3.movieweb.com/i/article/PnwErFzskr994GV595BjCw07wwEhOq/798:50/John-Wick-3-Ends-Movie-Franchise.jpg",
 		scores: [5,1,3,4,1,5,2,2,4,3],
+	},
+	{
+		name: "Rambo",
+		image: "https://cdn4.i-scmp.com/sites/default/files/styles/980x551/public/2015/03/13/c7e0e3b6df4dafdfd4061fe0c4b0e733.jpg?itok=X_x0N--3",
+		scores: [3,1,3,1,1,5,2,1,4,3],
+	},
+	{
+		name: "Captain Planet",
+		image: "https://captainplanetfoundation.org/wp-content/uploads/CaptainPlanet.png",
+		scores: [5,5,3,5,1,5,5,2,5,3],
 	},
 ];
 
